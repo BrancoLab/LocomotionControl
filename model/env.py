ba8800b2bed615bc3a7359d6015cfc9e35ad6746
FedeClaudi <federicoclaudi@protonmail.com>
2020-08-26 00:21:35 +0100

    bump

--- model/env.py
+++ model/env.py
@@ -21,41 +21,50 @@ class AlloEnv(Env):
 
 
     @staticmethod
-    def make_road(linelength=3., circle_radius=1.):
+    def make_road(linelength=1., circle_radius=1.):
         """ make track
         Returns:
             road (numpy.ndarray): road info, shape(n_point, 3) x, y, angle
         """
-        # line
-        # not include start points
-        line = np.linspace(-1.5, 1.5, num=51, endpoint=False)[1:]
-        line_1 = np.stack((line, np.zeros(50)), axis=1)
-        line_2 = np.stack((line[::-1], np.zeros(50)+circle_radius*2.), axis=1)
+        # # not include start points
+        # line = np.linspace(-linelength/2, linelength/2, 
+        #                         num=11, endpoint=False)[1:]
+        # line_1 = np.stack((line, np.zeros(10)), axis=1)
+        # line_2 = np.stack((line[::-1], np.zeros(10)+circle_radius*2.), axis=1)
 
-        # circle
-        circle_1_x, circle_1_y = circle(linelength/2., circle_radius,
-            circle_radius, start=-np.pi/2., end=np.pi/2., n_point=50)
-        circle_1 = np.stack((circle_1_x , circle_1_y), axis=1)
+        # # circle
+        # circle_1_x, circle_1_y = circle(linelength/2., circle_radius,
+        #     circle_radius, start=-np.pi/2., end=np.pi/2., n_point=50)
+        # circle_1 = np.stack((circle_1_x , circle_1_y), axis=1)
         
-        circle_2_x, circle_2_y = circle(-linelength/2., circle_radius,
-            circle_radius, start=np.pi/2., end=3*np.pi/2., n_point=50)
-        circle_2 = np.stack((circle_2_x , circle_2_y), axis=1)
+        # circle_2_x, circle_2_y = circle(-linelength/2., circle_radius,
+        #     circle_radius, start=np.pi/2., end=3*np.pi/2., n_point=50)
+        # circle_2 = np.stack((circle_2_x , circle_2_y), axis=1)
 
-        road_pos = np.concatenate((line_1, circle_1, line_2, circle_2), axis=0)
+        # road_pos = np.concatenate((line_1, circle_1, line_2, circle_2), axis=0)
 
-        # calc road angle
-        road_diff = road_pos[1:] - road_pos[:-1]
+        # # calc road angle
+        # road_diff = road_pos[1:] - road_pos[:-1]
         # road_angle = np.arctan2(road_diff[:, 1], road_diff[:, 0]) 
         # road_angle = np.concatenate((np.zeros(1), road_angle))
-        road_angle = calc_angle_between_points_of_vector_2d(road_pos[:, 0], road_pos[:, 1])
-        road_vel = np.ones_like(road_angle) * 50
+        # road_vel = np.ones_like(road_angle) + 2.5
 
-        road = np.concatenate((road_pos, road_angle[:, np.newaxis], road_vel[:, np.newaxis]), axis=1)
-        road =  np.tile(road, (3, 1)) 
 
-        import matplotlib.pyplot as plt
-        plt.plot(road[:, 0], road[:, 1])
-        plt.show()
+        # road = np.concatenate((road_pos, road_angle[:, np.newaxis], road_vel[:, np.newaxis]), axis=1)
+        # road =  np.tile(road, (3, 1))
+
+        x = np.linspace(0, 11, 101)
+        y = np.sin(x)
+        angle = np.radians(calc_angle_between_points_of_vector_2d(x, y))
+        v = np.ones_like(x) * 5
+
+        road = np.vstack([x, y, angle, v]).T
+
+        # ? plot road
+        # import matplotlib.pyplot as plt
+        # # plt.scatter(road[:, 0], road[:, 1], c=road[:, 2], vmin=0, vmax=np.pi, cmap='bwr')
+        # plt.scatter(x, y, c=np.degrees(angle), vmin=0, vmax=360, cmap='bwr')
+        # plt.show()
 
         return road
 
---
+++ model_orig/config.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class AlloConfig():
+
+
+    def __init__(self):
+        """ 
+        """
+        
+
+
+   
