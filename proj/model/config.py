c1ad8f56bdd3dae666d4e9237d7944fdd6b56a4a
FedeClaudi <federicoclaudi@protonmail.com>
2020-09-01 16:56:53 +0100

    well behaved compact model

--- proj/model/config.py
+++ proj/model/config.py
@@ -4,7 +4,7 @@ class Config:
     mouse = dict(
         L = 5, # half body width | cm
         R = 1, # radius of wheels | cm
-        d = 0, # distance between axel and CoM | cm
+        d = 0.2, # distance between axel and CoM | cm
         length = 6, # cm
         m = round(24/9.81, 2), # mass | g
         m_w = round(2/9.81, 2), # mass of wheels/legs |g
--- proj/model/model.py
+++ proj/model/model.py
@@ -8,33 +8,26 @@ from proj.model.config import Config
 from proj.utils import merge
 
 class Model(Config):
-    _D_args = ['R', 'd', 'm', 'm_w', 'omega', 'tau_r', 'tau_l', "L", 'v']
+    _M_args = ['theta', 'v', 'omega', 'L', 'R', 'm', 'd', 'm_w' , 'tau_l', 'tau_r']
 
     _control = namedtuple('control', 'tau_r, tau_l')
-    _eta = namedtuple('eta', 'v, omega')
-    _etadot = namedtuple('etadot', 'vdot, omegadot')
-    _state = namedtuple('state', 'x, y, theta, s')
+    _state = namedtuple('state', 'x, y, theta, v, omega')
 
     def __init__(self):
         Config.__init__(self)
 
         self._make_simbols()
-        self.get_dynamics()
-        self.get_kinematics()
+        self.get_combined_dynamics_kinematics()
         self.reset()
 
     def reset(self):
-        self.curr_x = self._state(0, 0, 0, 0)
-        self.curr_thetadot = {'thetadot': 0}
-        self.curr_eta = self._eta(0, 0)
-        self.curr_etadot = self._etadot(0, 0)
+        self.curr_x = self._state(0, 0, 0, 0, 0)
         self.curr_control = self._control(0, 0) # use only to keep track
 
         self.history = dict(
             x = [],
             y = [],
             theta = [],
-            s = [],
             v = [],
             omega = [],
             tau_r = [],
@@ -44,20 +37,17 @@ class Model(Config):
         self._append_history() # make sure first state is included
 
     def _append_history(self):
-        for ntuple in [self.curr_x, self.curr_eta, self.curr_control]:
+        for ntuple in [self.curr_x, self.curr_control]:
             for k,v in ntuple._asdict().items():
                 self.history[k].append(v)
 
+
     def _make_simbols(self):
         # state variables
-        x, y, theta, thetadot, s = symbols('x, y, theta, thetadot, s', real=True)
+        x, y, theta, thetadot = symbols('x, y, theta, thetadot', real=True)
 
         # static variables
         L, R, m, m_w, d = symbols('L, R, m, m_w, d', real=True)
-
-        # wheel speeds
-        psi_l, psi_r = symbols('psi_l, psi_r', real=True)
-        psidot_l, psidot_r = symbols('psidot_l, psidot_r', real=True)
 
         # control variables
         tau_r, tau_l = symbols('tau_r, tau_l', real=True)
@@ -66,12 +56,11 @@ class Model(Config):
         v, omega = symbols('v, omega', real=True)
         vdot, omegadot = symbols('vdot, omegadot', real=True)
 
+        # store symbols
         self.variables = dict(
             x = x,
             y = y,
             theta = theta,
-            thetadot = thetadot,
-            s = s,
 
             L = L,
             R = R,
@@ -79,119 +68,61 @@ class Model(Config):
             m_w = m_w,
             d = d,
 
-            psi_l=psi_l,
-            psi_r = psi_r,
-            psidot_l=psidot_l,
-            psidot_r = psidot_r,
-
             tau_l=tau_l,
             tau_r=tau_r,
 
             v=v,
             omega=omega,
-            vdot=vdot,
-            omegadot=omegadot,
         )
 
-    # def _make_matrices(self): 
-    #     x, y, theta, thetadot, s, L, R, m, d, psi_l, psi_r, psidot_l, \
-    #                 psidot_r, tau_l, tau_r, v, omega, vdot, omegadot = self.variables.values()
-        
-    #     # Define moments of inertia
-    #     I = 2 * m * d**2
 
-    #     # Define 
-    #     F = R**2/(4*L**2)
 
-    #     Q = Matrix([
-    #         [R/2 * cos(theta), R/2 * cos(theta)],
-    #         [R/2 * sin(theta), R/2 * sin(theta)],
-    #         [R/(2 * L), -R/(2*L)],
-    #         [R/(2 * L), R/(2*L)]
-    #     ])
-
-    #     M = Matrix([
-    #         [F*(m*L**2 + I), F*(m*L**2 - I)],
-    #         [F*(m*L**2 - I), F*(m*L**2 + I)]
-    #     ])
-
-    #     V = Matrix([
-    #         [0, R**2/(2*L)*m*d*thetadot],
-    #         [-R**2/(2*L)*m*d*thetadot, 0],
-    #     ])
-
-    #     tau = Matrix([tau_r, tau_l])
-    #     eta = Matrix([psi_r, psi_l])
-    #     etadot = Matrix([psidot_r, psidot_l])
-
-    #     self.matrixes = dict(
-    #         Q = Q,
-    #         M = M,
-    #         V = V,
-    #         eta = eta,
-    #         etadot = etadot,
-    #         tau = tau,
-    #     )
-
-    #     return Q, M, V, tau, eta, etadot
-
-    def get_dynamics(self):
-        x, y, theta, thetadot, s, L, R, m, m_w, d, psi_l, psi_r, psidot_l, \
-                psidot_r, tau_l, tau_r, v, omega, vdot, omegadot = self.variables.values()
-        self.matrixes = {}
+    def get_combined_dynamics_kinematics(self):
+        x, y, theta, L, R, m, m_w, d, tau_l, tau_r, v, omega = self.variables.values()
 
         # Define moments of inertia
         I_c = m * d**2 # mom. inertia around center of gravity
         I_w = m_w * R**2 # mom. inertia of wheels
         I = I_c + m*d**2 + 2*m_w*L**2 + I_w
 
-        # Define two diff equations
-        eq1 = Eq(m * vdot - m*d*omega**2, 1/R * (tau_r + tau_l))
-        eq2 = Eq(I*omegadot + m*d*omega*v, (L/R)*(tau_r - tau_l))
+        # Define a constant:
+        J = I + (2*I**2/R**2) * I_w
 
-        # solve
-        vdot_sol = solve(eq1, vdot)[0]
-        omegadot_sol = solve(eq2, omegadot)[0]
+        # Define g vector and input vector
+        g = Matrix([0, 0, 0, d*omega**2, - (m * d * omega * v)/J])
+        inp = Matrix([v, omega, tau_r, tau_l])
 
-        # store in matrix
-        D = Matrix([vdot_sol, omegadot_sol])
-        self.matrixes['D'] = D
-
-        # Create lambdified function
-        self.calc_D = lambdify([self.variables[k] for k in self._D_args], self.matrixes['D'])
-
-    def get_kinematics(self):
-        x, y, theta, thetadot, s, L, R, m, m_w, d, psi_l, psi_r, psidot_l, \
-                psidot_r, tau_l, tau_r, v, omega, vdot, omegadot = self.variables.values()
-
-        Q = Matrix([
-            [cos(theta), 0],
-            [sin(theta), 0],
-            [0, 1],
-            [1, 0]
+        # Define M matrix
+        M = Matrix([
+            [cos(theta), 0, 0, 0],
+            [sin(theta), 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, L/(m*R), L/(m*R)],
+            [0, 0, L/(J*R), -L/(J*R)]
         ])
-        self.matrixes['Q'] = Q
 
-        x = Q * Matrix([v, omega])
+        # vectorize expression
+        args = [theta, v, omega, L, R, m, d, m_w , tau_l, tau_r]
+        expr = g + M*inp
+        self.calc_dqdt = lambdify(args, expr, modules='numpy')
 
-        self.matrixes['Qeta'] = x
-
-        self.calc_Q = lambdify([theta, v, omega], x)
+        # store matrices
+        self.matrixes = dict(
+            g = g,
+            inp = inp,
+            M = M,
+        )
 
 
     def step(self, u):
-        # First update eta
-        variables = merge(u, self.curr_x, self.curr_thetadot, self.mouse, self.curr_eta, self.curr_etadot)
+        # Compute dxdt
+        variables = merge(u, self.curr_x, self.mouse)
+        inputs = [variables[a] for a in self._M_args]
+        dxdt = self.calc_dqdt(*inputs).ravel()
 
-        # dv, domega
-        d = self.calc_D(*[variables[a] for a in self._D_args]).ravel()
-        self.curr_etadot = self._etadot(*np.array(d))
-
-        self.curr_eta = self._eta(*np.array(self.curr_eta) + d * self.dt)
-
-        # Then use deta to compute the state
-        dxdt = self.calc_Q(variables['theta'], self.curr_eta.v, self.curr_eta.omega).ravel()
-        self.curr_x = self._state(*(np.array(self.curr_x) + dxdt * self.dt))
+        # Step
+        next_x = np.array(self.curr_x) + dxdt * self.dt
+        self.curr_x = self._state(*next_x)
 
         # Update history
         self._append_history()
