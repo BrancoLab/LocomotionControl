b66e3e7f1b7507564479e0c3b60667f9d9ae794d
FedeClaudi <federicoclaudi@protonmail.com>
2020-09-01 10:45:06 +0100

    bump

--- proj/model/config.py
+++ proj/model/config.py
@@ -4,7 +4,7 @@ class Config:
     mouse = dict(
         L = 5, # half body width | cm
         R = 1, # radius of wheels | cm
-        d = .2, # distance between axel and CoM | cm
+        d = 0.1, # distance between axel and CoM | cm
         length = 6, # cm
         m = round(24/9.81, 2), # mass | g
     )
