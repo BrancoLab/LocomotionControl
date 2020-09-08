63a0c5ffebb08e03200695cc7b127b7f8580221a
FedeClaudi <federicoclaudi@protonmail.com>
2020-09-08 14:45:20 +0100

    polarmodelwip

--- proj/__init__.py
+++ proj/__init__.py
@@ -1,5 +1,6 @@
 from proj.model.config import Config
 from proj.model.model import Model
+from proj.model.model_polar import ModelPolar
 from proj.run.runner import run_experiment
 from proj.run.runner_manual import run_manual
 from proj.environment.environment import Environment
