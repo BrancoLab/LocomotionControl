7772dbbe9620d02cfe5a7e8879428e60d8905a7a
FedeClaudi <federicoclaudi@protonmail.com>
2020-09-16 09:41:34 +0100

    bump

--- run_allocentric.py
+++ run_allocentric.py
@@ -1,37 +1,21 @@
-# # %%
+# %%
 
-# from proj import (
-#     Model,
-#     Environment,
-#     Controller,
-#     run_experiment,
-#     ModelPolar,
-#     # plot_trajectory,
-#     plot_trajectory_polar,
-#     run_manual,
-# )
+from proj import (
+    Model,
+    Environment,
+    Controller,
+    run_experiment,
+    # ModelPolar,
+)
 
 
-# # ! TODO  formula I_c for cube!
-# # energy considerations, you should need more force to accelerate when going faster?
+# ! TODO  formula I_c for cube!
+# TODO energy considerations, you should need more force to accelerate when going faster?
 
-# # TODO the LIVE PLOTTING CARTESIANN COORDINATES STUFF IS INVERTED
-
-
-# # model = ModelPolar()
-# model = Model()
-# env = Environment(model)
-# control = Controller(model)
-
-# # plot_trajectory_polar(env.reset())
+# model = ModelPolar()
+model = Model()
+env = Environment(model)
+control = Controller(model)
 
 
-# run_experiment(env, control, model, plot=False)
-
-
-# # %%
-# # print('\ndqdt\n', model.model)
-# # print('\njacobian state\n', model.model_jacobian_state)
-# # print('\njacobian input\n', model.model_jacobian_input)
-
-# # %%
+run_experiment(env, control, model, plot=False)
