4a5d899442e1b40df94179c223a025489539988f
FedeClaudi <federicoclaudi@protonmail.com>
2020-09-17 16:05:44 +0100

    animate cli

--- proj/animation/animate.py
+++ proj/animation/animate.py
@@ -1,19 +1,30 @@
-
-import os
 import subprocess
-from fcutils.video.utils import get_cap_from_images_folder, save_videocap_to_video
+from fcutils.video.utils import (
+    get_cap_from_images_folder,
+    save_videocap_to_video,
+)
+import click
 
-def animate_from_images(folder, savepath):    
-    print('Loading images')
+
+def animate_from_images(folder, savepath):
+    print("Loading images")
     cap = get_cap_from_images_folder(folder, img_format="%2d.png")
 
-    print('Saving to video')
-    save_videocap_to_video(cap, savepath, '.mp4')
+    print("Saving to video")
+    save_videocap_to_video(cap, savepath, ".mp4")
 
-    gifpath = savepath.replace('.mp4', '.gif')
-    print('To save the video as GIF, use: \n'+
-            f'ffmpeg -i "{savepath}" -f gif "{gifpath}"'
+    gifpath = savepath.replace(".mp4", ".gif")
+    print(
+        "To save the video as GIF, use: \n"
+        + f'ffmpeg -i "{savepath}" -f gif "{gifpath}"'
     )
-    subprocess.call(['ffmpeg', '-i', f'{savepath}', '-f',  'gif', f'{gifpath}'])
+    subprocess.call(["ffmpeg", "-i", f"{savepath}", "-f", "gif", f"{gifpath}"])
 
-    print(f'\n\n\n saved gif at: {gifpath}')
+    print(f"\n\n\n saved gif at: {gifpath}")
+
+
+@click.command()
+@click.argument("folder")
+@click.argument("savepath")
+def main(folder, savepath):
+    animate_from_images(folder, savepath)
