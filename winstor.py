c6f5252d5409fa8a0d5b9b2df5fe81ec1764bde5
FedeClaudi <federicoclaudi@protonmail.com>
2020-11-30 18:19:00 +0000

    bump

--- winstor.py
+++ winstor.py
@@ -3,7 +3,7 @@ import click
 
 
 @click.command()
-@click.option("--trialn", default=0, help="Trial number.")
+@click.option("--trialn", default=None, help="Trial number.")
 def main(trialn):
     Manager(winstor=True, trialn=trialn).run()
 
