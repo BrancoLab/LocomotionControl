122612afe2dcf3065486aca225f8d752ff08d843
FedeClaudi <federicoclaudi@protonmail.com>
2021-01-17 15:47:51 +0000

    bump

--- control/config.py
+++ control/config.py
@@ -5,12 +5,12 @@ px_to_cm = 1 / 8
 
 
 MANAGER_CONFIG = dict(
-    exp_name="more_waypoints", live_plot=False, use_fast=True,
+    exp_name="planning_ahead_straight", live_plot=False, use_fast=True,
 )
 
 TRAJECTORY_CONFIG = dict(
     traj_type="simulated",  # tracking or simulated
-    n_steps=500,
+    n_steps=1000,
     min_dist=5,  # when within this distance from end, stop
 )
 
@@ -28,7 +28,7 @@ CONTROL_CONFIG = dict(
     STATE_SIZE=5,
     INPUT_SIZE=2,
     ANGLE_IDX=2,  # state vector index which is angle, used to fit diff in
-    R=np.diag([1.0e-5, 1.0e-5]),  # control cost
+    R=np.diag([1.0e-6, 1.0e-6]),  # control cost
     Q=np.diag([30, 30, 30, 10, 0]),  # state cost | x, y, theta, v, omega
     Sf=np.diag([0, 0, 0, 0, 0]),  # final state cost
 )
@@ -36,14 +36,14 @@ CONTROL_CONFIG_FLAT = dict(  # for easy saving
     STATE_SIZE=5,
     INPUT_SIZE=2,
     ANGLE_IDX=2,  # state vector index which is angle, used to fit diff in
-    R=[1.0e-7, 1.0e-7],  # control cost
+    R=[1.0e-6, 1.0e-6],  # control cost
     Q=[30, 30, 30, 10, 0],  # state cost | x, y, theta, v, omega
     Sf=[0, 0, 0, 0, 0],  # final state cost
 )
 
 PLANNING_CONFIG = dict(  # params used to compute goal states to be used for control
-    prediction_length=50,
-    n_ahead=5,  # start prediction states from N steps ahead
+    prediction_length=100,
+    n_ahead=25,  # start prediction states from N steps ahead
 )
 
 iLQR_CONFIG = dict(
