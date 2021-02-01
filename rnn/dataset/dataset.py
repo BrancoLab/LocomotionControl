c31d5a4bd92af9f1376963bb1d6ee398fddb44fd
FedeClaudi <federicoclaudi@protonmail.com>
2021-02-01 11:45:21 +0000

    bump

--- rnn/dataset/dataset.py
+++ rnn/dataset/dataset.py
@@ -36,8 +36,40 @@ class PredictTauFromDeltaXYT(Dataset, Preprocessing):
         return history.tau_r, history.tau_l
 
 
+class PredictPNNFromDeltaXYT(Dataset, Preprocessing):
+    description = """
+        Predict neural controls (P, N_L, N_R) from 
+        the  trajectory delta: difference betwen current state
+        and next goal in the control simulation.
+
+        Data are normalized in range (-1, 1) with a MinMaxScaler for each
+        variable independently.
+    """
+
+    name = "dataset_predict_PNN_from_deltaXYT"
+    inputs_names = ("x", "y", "theta")
+    outputs_names = ("P", "N_R", "N_L")
+
+    def __init__(self, *args, truncate_at=None, **kwargs):
+        Preprocessing.__init__(self, truncate_at=truncate_at, **kwargs)
+        Dataset.__init__(self, *args, **kwargs)
+
+    def get_inputs(self, trajectory, history):
+        dx = history.goal_x - history.x
+        dy = history.goal_y - history.y
+        dtheta = history.goal_theta - history.theta
+
+        return dx, dy, dtheta
+
+    def get_outputs(self, history):
+        return history.P, history.N_r, history.N_l
+
+
 if __name__ == "__main__":
-    datasets = (PredictTauFromDeltaXYT,)
+    datasets = (
+        # PredictTauFromDeltaXYT,
+        PredictPNNFromDeltaXYT,
+    )
 
     for dataset in datasets:
         dataset().make()
