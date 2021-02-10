35ff33b9cdcde7f777a6a07ecfb7aef400a3c806
FedeClaudi <federicoclaudi@protonmail.com>
2021-02-10 14:42:31 +0000

    bump

---
+++ data_wrangling/frames.py
@@ -0,0 +1,19 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# from fcutils import video
+
+
+# cap = video.get_cap_from_file(
+#     r"Z:\swc\branco\Federico\Locomotion\control\behavioural_data\dario\14072020_BAA_1098162a_4.mp4"
+# )
+# frame = video.get_cap_selected_frame(cap, 1)
+
+
+# # plt.imshow(frame.astype(np.int64))
+# # plt.show()
+
+# pxs = 430
+# cm = 93
+
+# print(cm / pxs, pxs / cm)
