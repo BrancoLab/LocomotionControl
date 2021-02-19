a04239668f163dd64e709d221a0e2a2d384a242a
FedeClaudi <federicoclaudi@protonmail.com>
2021-02-19 17:14:12 +0000

    bump

--- winstor.py
+++ winstor.py
@@ -1,6 +1,9 @@
 from control.manager import Manager
 import click
+import shutil
 from pyinspect import install_traceback
+from loguru import logger
+
 
 install_traceback(
     keep_frames=5, all_locals=True, relevant_only=False,
@@ -13,9 +16,18 @@ install_traceback(
 def main(trialn, config):
     if trialn is not None:
         trialn = int(trialn)
-    Manager(winstor=True, trialn=trialn, config_file=config, to_db=False).run(
-        n_secs=4
-    )
+
+    try:
+        manager = Manager(
+            winstor=True, trialn=trialn, config_file=config, to_db=False
+        ).run(n_secs=4)
+    except Exception as e:
+        print(e)
+        try:
+            logger.remove()
+            shutil.rmtree(manager.datafolder)
+        except Exception as e:
+            print(e)
 
 
 if __name__ == "__main__":
