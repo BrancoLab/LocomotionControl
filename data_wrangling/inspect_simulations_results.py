38abf4ce3210c402895e6a37d999204409431cc9
FedeClaudi <federicoclaudi@protonmail.com>
2021-03-22 09:18:40 +0000

    bump

--- data_wrangling/inspect_simulations_results.py
+++ data_wrangling/inspect_simulations_results.py
@@ -1,17 +1,19 @@
 # %%
-from pigeon import annotate
-from IPython.display import display, Image
+# from pigeon import annotate
+# from IPython.display import display, Image
 from pathlib import Path
 from loguru import logger
+import numpy as np
 import sys
 import os
 
-module_path = Path(os.path.abspath(os.path.join("."))).parent.parent
+module_path = Path(os.path.abspath(os.path.join("."))).parent
 
 from fcutils import path
 from fcutils.progress import track
 
 sys.path.append(str(module_path))
+sys.path.append("./")
 from control.history import load_results_from_folder
 
 """
@@ -30,9 +32,7 @@ from control.history import load_results_from_folder
 """
 
 
-fld = Path(
-    "D:\\Dropbox (UCL)\\Rotation_vte\\Locomotion\\RNN\\training_data_temp"
-)
+fld = Path(r"Z:\swc\branco\Federico\Locomotion\control\data - Copy")
 flds = path.subdirs(fld)
 if flds is None:
     raise ValueError("Did not find any simulations folders")
@@ -40,26 +40,73 @@ logger.info(f"Found {len(flds)} folders")
 
 # %%
 
+
+def get_trajectory_waypoint_at_frame(history, trajectory):
+    """
+        Get trajectory waypoint at each frame
+    """
+    return np.vstack([trajectory[w, :] for w in history.trajectory_idx])
+
+
+def endpoint_distance(history, trajectory):
+    """
+        Distance between history end point and trajectory end point
+    """
+    return np.linalg.norm(
+        history[["x", "y"]].values[-1, :2] - trajectory[-1, :2]
+    )
+
+
+def max_trajectory_distance(history, trajectory):
+    """
+        Get max distance from trajectory during simulation
+    """
+    htraj = get_trajectory_waypoint_at_frame(history, trajectory)
+    return np.max(np.linalg.norm(htraj[:, :2] - history[["x", "y"]].values))
+
+
+# %%
 # ---------------------------------------------------------------------------- #
 #                              1. DISCARD FAILURES                             #
 # ---------------------------------------------------------------------------- #
+
+MAX_DIST = 15  # cm, max distance from trajectory
+END_POINT_DIST_TH = 8  # cm, distance between history and traj ends
+
 logger.info("Step 1: remove catastrophic failures")
 to_discard = []
-for subfld in track(flds):
+for n, subfld in track(enumerate(flds), total=len(flds)):
     # get the proprtion of trajectory waypoints visited
     try:
         history, info, trajectory, _ = load_results_from_folder(subfld)
     except FileNotFoundError:
-        logger.debug(f"Skipped {subfld}")
+        logger.debug(f"Skipped {subfld.name}")
         to_discard.append(subfld)
         continue
 
-    prop_visited = len(history.trajectory_idx.unique()) / trajectory.shape[0]
-
-    if prop_visited < 0.8:
+    if not len(history) or not len(trajectory):
+        logger.debug(f"{subfld.name} has empty history")
         to_discard.append(subfld)
+        continue
+
+    # check end point distance
+    if endpoint_distance(history, trajectory) > END_POINT_DIST_TH:
+        logger.debug(f"{subfld.name} end point distance too large")
+        to_discard.append(subfld)
+        continue
+
+    # check max distance
+    if max_trajectory_distance(history, trajectory) > MAX_DIST:
+        logger.debug(f"{subfld.name} max distance too large")
+        to_discard.append(subfld)
+        continue
+
 
 logger.info(f"Found {len(to_discard)}/{len(flds)} simulations to discard.")
+
+
+# %%
+# delete bad folders
 for subfld in to_discard:
     try:
         path.delete(subfld)
@@ -74,35 +121,35 @@ flds = path.subdirs(fld)
 #                                2. MANUAL PASS                                #
 # ---------------------------------------------------------------------------- #
 
-# get paths to all images
-images = {
-    fld: path.files(fld, "outcome.png")
-    for fld in flds
-    if path.files(fld, "outcome.png")
-}
-lookup = {v: k for k, v in images.items()}
+# # get paths to all images
+# images = {
+#     fld: path.files(fld, "outcome.png")
+#     for fld in flds
+#     if path.files(fld, "outcome.png")
+# }
+# lookup = {v: k for k, v in images.items()}
 
 
-# do a first round of manual selection
-annotations = annotate(
-    images.values(),
-    options=["good", "bad"],
-    display_fn=lambda filename: display(Image(filename)),
-)
+# # do a first round of manual selection
+# annotations = annotate(
+#     images.values(),
+#     options=["good", "bad"],
+#     display_fn=lambda filename: display(Image(filename)),
+# )
 
-# %%
-annotations_dict = {k: v for (k, v) in annotations}
-_images = {
-    fld: img for fld, img in images.items() if img in annotations_dict.keys()
-}
-discard = {
-    fld: img for fld, img in _images.items() if annotations_dict[img] == "bad"
-}
-logger.info(f"Discarded {len(discard)}/{len(images)} simulations")
+# # %%
+# annotations_dict = {k: v for (k, v) in annotations}
+# _images = {
+#     fld: img for fld, img in images.items() if img in annotations_dict.keys()
+# }
+# discard = {
+#     fld: img for fld, img in _images.items() if annotations_dict[img] == "bad"
+# }
+# logger.info(f"Discarded {len(discard)}/{len(images)} simulations")
 
-for subfld in discard.keys():
-    path.delete(subfld)
-flds = path.subdirs(fld)
-logger.info(f"We are left with {len(flds)} folders")
+# for subfld in discard.keys():
+#     path.delete(subfld)
+# flds = path.subdirs(fld)
+# logger.info(f"We are left with {len(flds)} folders")
 
-# %%
+# # %%
--- data_wrangling/prepare_launch_all_bash_info.py
+++ data_wrangling/prepare_launch_all_bash_info.py
@@ -17,5 +17,5 @@ this tells you how many trials are ready in the cache for simulations
 trials = pd.read_hdf(paths.trials_cache, key="hdf")
 print(f"[salmon]Loaded {len(trials)} trials from : '{paths.trials_cache}'")
 print(
-    f"[salmon]To run on HPC: --[b] bash launch_all_trials.sh {int(np.floor(len(trials)/500))} [/b]--which will create N jobs each running 500 trials"
+    f"[salmon]To run on HPC: --[b] bash launch_all_trials.sh {int(np.floor(len(trials)/50))} [/b]--which will create N jobs each running 500 trials"
 )
