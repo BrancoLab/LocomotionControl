754e14d80e91eb843774d375da0345ab4c911c8a
FedeClaudi <federicoclaudi@protonmail.com>
2021-07-20 10:07:58 +0100

    probe validation working well

--- data/dbase/quality_control.py
+++ data/dbase/quality_control.py
@@ -6,6 +6,7 @@ import sys
 sys.path.append("./")
 
 import matplotlib.pyplot as plt
+from myterial import blue_dark, salmon_dark, indigo
 
 from fcutils.video import get_video_params
 from fcutils.maths.signals import get_onset_offset
@@ -13,7 +14,7 @@ from fcutils.maths.signals import get_onset_offset
 from data.dbase._tables import load_bin
 
 
-def validate_bonsai(video_file_path, ai_file_path, analog_sampling_rate):
+def validate_behavior(video_file_path, ai_file_path, analog_sampling_rate):
     """
         Checks that a bonsai behaviour recording ran correctly
         (i.e. no frames dropped)
@@ -60,6 +61,7 @@ def validate_bonsai(video_file_path, ai_file_path, analog_sampling_rate):
             )
             return False, nsigs
     else:
+        nsigs = 4
         logger.debug(
             f"{name} has {nframes} frames and {len(frame_trigger_times)} trigger times were found"
         )
@@ -79,6 +81,153 @@ def validate_bonsai(video_file_path, ai_file_path, analog_sampling_rate):
     return True, nsigs
 
 
+# ---------------------------------------------------------------------------- #
+#                              validate ephys data                             #
+# ---------------------------------------------------------------------------- #
+def plot_recording_triggers(
+    bonsai_sync,
+    ephys_sync,
+    bonsai_sync_onsets,
+    bonsai_sync_offsets,
+    ephys_sync_onsets,
+    ephys_sync_offsets,
+    sampling_rate,
+    time_scaling_factor,
+):
+    N = 50
+    bonsai_time = (
+        np.arange(0, len(bonsai_sync), step=N) - bonsai_sync_onsets[0]
+    ) / sampling_rate
+    probe_time = (
+        (np.arange(0, len(ephys_sync), step=N) - ephys_sync_onsets[0])
+        / sampling_rate
+        * time_scaling_factor
+    )
+
+    # plot sync signals
+    f, axes = plt.subplots(
+        figsize=(16, 8), ncols=2, gridspec_kw={"width_ratios": [3, 1]}
+    )
+    axes[0].plot(bonsai_time, bonsai_sync[::N] / 5, lw=0.5, color="b")
+    axes[0].plot(probe_time, ephys_sync[::N] * 0.02, lw=0.5, color="salmon")
+
+    # mark trigger onsets
+    bad_onsets = np.where(np.diff(bonsai_sync_onsets) != sampling_rate)[0]
+    probe_bad_onsets = np.where(
+        np.abs(np.diff(ephys_sync_onsets) - sampling_rate) > sampling_rate / 2
+    )[0]
+
+    axes[0].scatter(
+        (bonsai_sync_onsets[bad_onsets] - bonsai_sync_onsets[0])
+        / sampling_rate,
+        np.ones_like(bad_onsets) * 1.4,
+        marker="v",
+        color=indigo,
+        alpha=0.75,
+        s=75,
+        zorder=120,
+    )
+    axes[0].scatter(
+        (bonsai_sync_onsets - bonsai_sync_onsets[0]) / sampling_rate,
+        np.ones_like(bonsai_sync_onsets) * 1.2,
+        marker="v",
+        color=blue_dark,
+        zorder=100,
+    )
+
+    axes[0].scatter(
+        (ephys_sync_onsets - ephys_sync_onsets[0])
+        / sampling_rate
+        * time_scaling_factor,
+        np.ones_like(ephys_sync_onsets) * 1.25,
+        marker="v",
+        color=salmon_dark,
+        zorder=100,
+    )
+    axes[0].scatter(
+        (ephys_sync_onsets[probe_bad_onsets] - ephys_sync_onsets[0])
+        / sampling_rate
+        * time_scaling_factor,
+        np.ones_like(probe_bad_onsets) * 1.45,
+        marker="v",
+        s=85,
+        alpha=0.7,
+        color=salmon_dark,
+        zorder=100,
+    )
+
+    axes[0].set(title="blue: bonsai | salmon: probe", xlabel="time (s)")
+
+    # plot histogram of triggers durations
+    axes[1].hist(np.diff(bonsai_sync_onsets), bins=50, color="b", alpha=0.5)
+    axes[1].hist(
+        np.diff(ephys_sync_onsets), bins=50, color="salmon", alpha=0.5
+    )
+    axes[1].set(title="blue: bonsai | salmon: probe", xlabel="offset duration")
+
+
+def get_onsets_offsets(bonsai_probe_sync, ephys_probe_sync, sampling_rate):
+    logger.debug("extracting sync signal pulses")
+    bonsai_sync_onsets, bonsai_sync_offsets = get_onset_offset(
+        bonsai_probe_sync, 2.5
+    )
+    ephys_sync_onsets, ephys_sync_offsets = get_onset_offset(
+        ephys_probe_sync, 45
+    )
+
+    # remove pulses that are too brief
+    errors = np.where(np.diff(bonsai_sync_onsets) < sampling_rate / 3)[0]
+    bonsai_sync_offsets = np.delete(bonsai_sync_offsets, errors)
+    bonsai_sync_onsets = np.delete(bonsai_sync_onsets, errors)
+
+    # check if numbers make sense
+    if len(bonsai_sync_onsets) != len(bonsai_sync_offsets):
+        raise ValueError(
+            f"BONSAI - Unequal number of onsets/offsets ({len(bonsai_sync_offsets)}/{len(bonsai_sync_onsets)})"
+        )
+    if len(ephys_sync_onsets) != len(ephys_sync_offsets):
+        raise ValueError(
+            f"EPHYS - Unequal number of onsets/offsets ({len(ephys_sync_offsets)}/{len(ephys_sync_onsets)})"
+        )
+
+    # check same results for bonsai and ephys
+    if len(bonsai_sync_onsets) != len(ephys_sync_onsets):
+        logger.error(
+            f"Incosistent number of triggers! Bonsai {len(bonsai_sync_onsets)} and SpikeGLX {len(ephys_sync_onsets)}"
+        )
+        raise ValueError(
+            "When inspecting probe sync signal found different number of pulses for bonsai "
+            f"{len(bonsai_sync_onsets)} and SpikeGLX {len(ephys_sync_onsets)}"
+        )
+    else:
+        logger.debug(
+            f"[green]Both bonsai and spikeGLX have {len(ephys_sync_onsets)} sync pulses"
+        )
+
+    if ephys_sync_onsets[0] <= bonsai_sync_onsets[0]:
+        raise ValueError("Bonsai should start first!")
+
+    # check the interval between sync signals in bonsai
+    onsets_delta = np.diff(bonsai_sync_onsets)
+    if len(set(onsets_delta)) > 1:
+        counts = {
+            k: len(onsets_delta[onsets_delta == k]) for k in set(onsets_delta)
+        }
+        logger.warning(f"Bonsai sync triggers have variable delay: {counts}")
+    elif list(onsets_delta)[0] != sampling_rate:
+        # check that it lasts as long as it should
+        raise ValueError(
+            f"Bonsai sync triggers are not 1s apart (got {list(onsets_delta)[0]} instead of {sampling_rate})"
+        )
+
+    return (
+        bonsai_sync_onsets,
+        bonsai_sync_offsets,
+        ephys_sync_onsets,
+        ephys_sync_offsets,
+    )
+
+
 def validate_recording(
     ai_file_path, ephys_ap_data_path, debug=False, sampling_rate=30000
 ):
@@ -92,113 +241,80 @@ def validate_recording(
             ephys_ap_data_path: str. Path to .bin with AP ephys data.
     """
     name = Path(ai_file_path).name
-    logger.debug(f"Running validate recordings on {name}")
+    logger.debug(f"\nRunning validate recordings on {name}")
 
     # load analog from bonsai
     analog = load_bin(ai_file_path, nsigs=4)
-    bonsai_probe_sync = analog[:, 3]
+    bonsai_probe_sync = analog[:, 3].copy()
 
     # load data from ephys
     ephys = load_bin(ephys_ap_data_path, nsigs=385, order="F", dtype="int16")
-    ephys_probe_sync = ephys[:, -1]
+    ephys_probe_sync = ephys[:, -1].copy()
+
+    # check for aberrant signals in bonsai
+    errors = np.where((bonsai_probe_sync != 0) & (bonsai_probe_sync != 1))[0]
+    if len(errors):
+        logger.warning(
+            f"Found {len(errors)} samples with too high values in probe signal"
+        )
+    bonsai_probe_sync[errors] = bonsai_probe_sync[errors - 1]
+
+    # check for aberrant signals in ephys
+    errors = np.where(ephys_probe_sync > 70)[0]
+    if len(errors):
+        logger.warning(
+            f"Found {len(errors)} samples with too high values in probe signal"
+        )
+    ephys_probe_sync[errors] = ephys_probe_sync[errors - 1]
+
+    # close stuff to save memory
     logger.debug(
         f"{name}  | Bonsai analogs shape: {analog.shape} | ephys data shape: {ephys.shape}"
     )
+    del ephys
+    del analog
 
-    # find probe syn pulses in both
-    bonsai_sync_onsets = get_onset_offset(bonsai_probe_sync, 2.5)[0]
-    ephys_sync_onsets = get_onset_offset(ephys_probe_sync, 30)[0]
+    # find probe sync pulses in both
+    (
+        bonsai_sync_onsets,
+        bonsai_sync_offsets,
+        ephys_sync_onsets,
+        ephys_sync_offsets,
+    ) = get_onsets_offsets(bonsai_probe_sync, ephys_probe_sync, sampling_rate)
 
-    if len(bonsai_sync_onsets) != len(ephys_sync_onsets):
-        raise ValueError(
-            "When inspecting probe sync signal found different number of pulses for bonsai "
-            f"{len(bonsai_sync_onsets)} and SpikeGLX {len(ephys_sync_onsets)}"
-        )
-    else:
-        logger.debug(
-            f"[green]Both bonsai and spikeGLX have {len(ephys_sync_onsets)} sync pulses"
-        )
-
-    if ephys_sync_onsets[0] <= bonsai_sync_onsets[0]:
-        raise ValueError("Bonsai should start first!")
-
-    # check the interval between syn signals in bonsai
-    onsets_delta = set(np.diff(bonsai_sync_onsets))
-    if len(onsets_delta) > 1:
-        raise ValueError(
-            f"Bonsai sync triggers have variable delay: {onsets_delta}"
-        )
-    elif list(onsets_delta)[0] != sampling_rate:
-        raise ValueError(
-            f"Bonsai sync triggers are not 1s apart (got {list(onsets_delta)[0]} instead of {sampling_rate})"
-        )
+    # get time scaling factor
+    time_scaling_factor = 1 / (
+        (ephys_sync_onsets[-1] - ephys_sync_onsets[0])
+        / (bonsai_sync_onsets[-1] - bonsai_sync_onsets[0])
+    )
 
     # debugging plots
     if debug:
-        frames_cut = analog[
-            bonsai_sync_onsets[0] : bonsai_sync_onsets[-1] + sampling_rate + 1,
-            0,
-        ]
-        bonsai_cut = bonsai_probe_sync[
-            bonsai_sync_onsets[0]
-            - 1 : bonsai_sync_onsets[-1]
-            + sampling_rate
-            + 1
-        ]
-        ephys_cut = ephys_probe_sync[
-            ephys_sync_onsets[0]
-            - 1 : ephys_sync_onsets[-1]
-            + sampling_rate
-            + 1
-        ]
-
-        f, axes = plt.subplots(ncols=2)
-        axes[0].plot(
-            frames_cut[: 20 * sampling_rate] * 0.5,
-            color="b",
-            label="bonsai frames",
+        plot_recording_triggers(
+            bonsai_probe_sync,
+            ephys_probe_sync,
+            bonsai_sync_onsets,
+            bonsai_sync_offsets,
+            ephys_sync_onsets,
+            ephys_sync_offsets,
+            sampling_rate,
+            time_scaling_factor,
         )
-        axes[0].plot(
-            bonsai_cut[: 20 * sampling_rate], color="k", label="bonsai"
-        )
-        axes[0].plot(
-            ephys_cut[: 20 * sampling_rate] / 10, color="g", label="ephys"
-        )
-        axes[0].legend()
-
-        axes[1].plot(
-            frames_cut[-40 * sampling_rate :] * 0.5,
-            color="b",
-            label="bonsai frames",
-        )
-        axes[1].plot(
-            bonsai_cut[-40 * sampling_rate :], color="k", label="bonsai"
-        )
-        axes[1].plot(
-            ephys_cut[-40 * sampling_rate :] / 10, color="g", label="ephys"
-        )
-        axes[1].legend()
-
-        logger.debug("Showing plot")
-        plt.show()
-
-    return (
-        bonsai_sync_onsets[0],
-        bonsai_sync_onsets[-1] + sampling_rate,
-        ephys_sync_onsets[-1],
-        ephys_sync_onsets[-1] + sampling_rate,
-    )
 
 
 if __name__ == "__main__":
     fld = Path("J:\\test_data")
 
     # ran quality control on test files
-    vid = fld / "FC_210505_AAA111075_recording_test_run_video.avi"
-    ai = fld / "FC_210505_AAA111075_recording_test_run_analog.bin"
+    vid = fld / ("FC_210714_AAA1110751_r4_hairpin" + "_video.avi")
+    ai = fld / ("FC_210714_AAA1110751_r4_hairpin" + "_analog.bin")
     sampling_rate = 30000
 
-    ephis_ap = fld / "test_run_g0_t0.imec0.ap.bin"
+    ephis_ap = fld / (
+        "210714_750_longcol_intref_hairpin" + "_g0_t0.imec0.ap.bin"
+    )
 
-    # validate_bonsai(vid, ai, sampling_rate)
+    # validate_behavior(vid, ai, sampling_rate)
     validate_recording(ai, ephis_ap, debug=True)
+
+    plt.show()
