66489c86c7ea5c0ec234cce87d03c5662582024c
FedeClaudi <federicoclaudi@protonmail.com>
2021-11-08 22:31:35 +0000

    bump

--- data/save_to_local.py
+++ data/save_to_local.py
@@ -20,20 +20,10 @@ recorder.start(
     folder_name='saved_data', timestamp=False
 )
 
-# ---------------------------------------------------------------------------- #
-#                               download tracking                              #
-# ---------------------------------------------------------------------------- #
+save_folder = Path(r'D:\Dropbox (UCL)\Rotation_vte\Locomotion\analysis\behavior\saved_data')
 
-# add tracking
 tracking = {}
-sessions = list(set(ROICrossing().fetch('name')))
-logger.info(f'Getting tracking data for {len(sessions)} sessions')
-for n, session in track(enumerate(sessions), total=len(sessions)):
-    tracking[session] = Tracking.get_session_tracking(session, movement=False, body_only=False)
-    time.sleep(.5)
 
-    # if n > 2:
-    #     break
 
 # ---------------------------------------------------------------------------- #
 #                              save roi crossings                              #
@@ -49,40 +39,59 @@ for ROI in arena.ROIs_dict.keys():
     )
     logger.info(f"Loaded {len(crossings)} crossings for: '{ROI}'")
 
-    roi_crossing_dict = []
     for i, crossing in track(crossings.iterrows(), total=len(crossings)):
+        save_name = f'crossing_{ROI}_{i}.json'
+        if (save_folder / save_name).exists():
+            continue
+
+        if crossing['name'] not in tracking.keys():
+                tracking[crossing['name']] = Tracking.get_session_tracking(crossing['name'], movement=False, body_only=False)
+                time.sleep(1)
+
         crossing_trk = ROICrossing.get_crossing_tracking(crossing, tracking[crossing['name']])
-        roi_crossing_dict.append({**crossing.to_dict(), **crossing_trk})
+        roi_crossing_dict = {**crossing.to_dict(), **crossing_trk}
+        
+        for k,v in roi_crossing_dict.items():
+            if isinstance(v, np.ndarray):
+                roi_crossing_dict[k] = list(v)
+        
+        recorder.add_data(roi_crossing_dict, f'crossing_{ROI}_{i}', fmt="json")
 
     recorder.add_data(crossings, f"{ROI}_crossings", fmt="h5")
-    recorder.add_data(roi_crossing_dict, f"{ROI}_crossings", fmt="json")
 
 
 # ---------------------------------------------------------------------------- #
 #                             save locomotor bouts                             #
 # ---------------------------------------------------------------------------- #
-logger.info('Fetching bouts')
-bouts = pd.DataFrame(
-    (
-        LocomotionBouts
-        & 'complete="true"'
-        & 'direction="outbound"'
-    ).fetch()
-)
-logger.info(f'Got {len(bouts)} bouts')
+# logger.info('Fetching bouts')
+# bouts = pd.DataFrame(
+#     (
+#         LocomotionBouts
+#         & 'complete="true"'
+#         & 'direction="outbound"'
+#     ).fetch()
+# )
+# logger.info(f'Got {len(bouts)} bouts')
 
 
-bouts_dicts = []
-for i, bout in track(bouts.iterrows(), total=len(bouts)):
-    trk = LocomotionBouts.get_bout_tracking(bout, tracking[bout['name']])
-    trk = {bp:np.array(list(v.values())) for bp, v in trk.to_dict().items()}
+# bouts_dicts = []
+# for i, bout in track(bouts.iterrows(), total=len(bouts)):
+#     save_name = f'{i}_complete_bout.json'
+#     if (save_folder / save_name).exists():
+#         continue
+#     else:
+#         if bout['name'] not in tracking.keys():
+#                 tracking[bout['name']] = Tracking.get_session_tracking(bout['name'], movement=False, body_only=False)
+#                 time.sleep(1)
+                
+#         trk = LocomotionBouts.get_bout_tracking(bout, tracking[bout['name']])
+#         trk = {bp:np.array(list(v.values())) for bp, v in trk.to_dict().items()}
 
-    bout_dict = {**bout.to_dict(), **trk}
-    for k,v in bout_dict.items():
-        if isinstance(v, np.ndarray):
-            bout_dict[k] = list(v)
+#         bout_dict = {**bout.to_dict(), **trk}
+#         for k,v in bout_dict.items():
+#             if isinstance(v, np.ndarray):
+#                 bout_dict[k] = list(v)
 
-    bouts_dicts.append(bout_dict)
     
-recorder.add_data(bouts_dicts, f"complete_bouts", fmt="json")
-recorder.describe()
+#         recorder.add_data(bout_dict, f'{i}_complete_bout', fmt="json")
+# recorder.describe()
