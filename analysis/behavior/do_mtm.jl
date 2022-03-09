9c6e9c03e3bc9c95f179d9c3ace0d5cabe162402
T-C. Kao <tck29@cblgpu02.eng.cam.ac.uk>
2022-03-09 18:13:07 +0000

    minor changes to visuals

--- analysis/behavior/do_mtm.jl
+++ analysis/behavior/do_mtm.jl
@@ -1,3 +1,4 @@
+module Run
 # using Revise
 # Revise.revise()
 
@@ -18,68 +19,72 @@ using jcontrol
 #             trajectory over time.
 #             """))
 
-# ----------------------------------- params --------------------------------- #
-DO_MODEL = true
-DO_FORWWARD = true
+function run(num_supports)
 
-δt = 0.01 # Δt for forward integration
+    # ----------------------------------- params --------------------------------- #
+    DO_MODEL = true
+    DO_FORWWARD = true
 
-# ---------------------------------------------------------------------------- #
-#                                DEFINE PARAMERS                               #
-# ---------------------------------------------------------------------------- #
-# create track object
-track = Track(; width=2, keep_n_waypoints=-1, resolution=0.001)
-@info "track" track
+    δt = 0.01 # Δt for forward integration
 
-# create bike
-bike = Bicycle(; L=6, l=2)
+    # ---------------------------------------------------------------------------- #
+    #                                DEFINE PARAMERS                               #
+    # ---------------------------------------------------------------------------- #
+    # create track object
+    track = Track(; width=2, keep_n_waypoints=-1, resolution=0.001)
+    @info "track" track
 
-coptions = ControlOptions(;
-    # solver optionsx
-    verbose=0,
-    n_iter=200,
-    num_supports=1500,
+    # create bike
+    bike = Bicycle(; L=6, l=2)
 
-    # error bounds
-    track_safety=0.75,
-    ψ_bounds=Bounds(-35, 35, :angle),
+    coptions = ControlOptions(;
+        # solver optionsx
+        verbose=0,
+        n_iter=200,
+        num_supports=num_supports,
 
-    # controls & variables bounds
-    uv_bounds=Bounds(-10, 10),          # cm/s²
-    uδ_bounds=Bounds(-1.5, 1.5),        # rad/s²
-    v_bounds=Bounds(5, 100),           # cm
-    δ_bounds=Bounds(-80, 80, :angle),   # deg
-)
+        # error bounds
+        track_safety=0.75,
+        ψ_bounds=Bounds(-35, 35, :angle),
 
-# define initial and final conditions
-icond = State(; x=track.X[1], y=track.Y[1], θ=track.θ[1], v=coptions.v_bounds.lower)
+        # controls & variables bounds
+        uv_bounds=Bounds(-10, 10),          # cm/s²
+        uδ_bounds=Bounds(-1.5, 1.5),        # rad/s²
+        v_bounds=Bounds(5, 100),           # cm
+        δ_bounds=Bounds(-80, 80, :angle),   # deg
+    )
 
-fcond = State(; v=coptions.v_bounds.lower)
+    # define initial and final conditions
+    icond = State(; x=track.X[1], y=track.Y[1], θ=track.θ[1], v=coptions.v_bounds.lower)
+    fcond = State(; v=coptions.v_bounds.lower)
 
-# ---------------------------------------------------------------------------- #
-#                                   FIT MODEL                                  #
-# ---------------------------------------------------------------------------- #
+    # ---------------------------------------------------------------------------- #
+    #                                   FIT MODEL                                  #
+    # ---------------------------------------------------------------------------- #
 
-if DO_MODEL
-    @info "control options" coptions
+    if DO_MODEL
+        @info "control options" coptions
 
-    control_model = create_and_solve_control(track, bike, coptions, icond, fcond)
+        control_model = create_and_solve_control(track, bike, coptions, icond, fcond)
+    end
+
+    # visualize results
+    summary_plot(control_model, :time)
+
+    # ---------------------------------------------------------------------------- #
+    #                              FORWARD INTEGRATION                             #
+    # ---------------------------------------------------------------------------- #
+    if DO_FORWWARD
+        solution = run_forward_model(control_model, icond, bike; δt=δt)
+    end
+
+    # visualize
+    summary_plot(solution, control_model, track, bike)
+
+    # print("\n")
+    # print(hLine("done"; style="bold blue"))
+    # print("\n\n")
+    #
+    return nothing
 end
-
-# visualize results
-summary_plot(control_model, :time)
-
-# ---------------------------------------------------------------------------- #
-#                              FORWARD INTEGRATION                             #
-# ---------------------------------------------------------------------------- #
-if DO_FORWWARD
-    solution = run_forward_model(control_model, icond, bike; δt=δt)
 end
-
-# visualize
-summary_plot(solution, control_model, track, bike)
-
-# print("\n")
-# print(hLine("done"; style="bold blue"))
-# print("\n\n")
-#
